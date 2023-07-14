b19c95c346b1401c2bf68a6c02efebfd37cc3692
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2023-07-14 15:51:04 +0530

    header updated

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import {BiPhoneCall} from  'react-icons/bi';
+import {AiOutlineMail}  from 'react-icons/ai'
 
 const header = () => {
   return <>
+  <header className='header-top-strip p-2 bg-danger'>
+<div className="container-xxl">
+  <div className="row">
+    <div className="col-6 text-white animated-right-left"  >
+      <p>Shipping available with Additional charges of $10.</p>
+      
+    </div>
+    <div className="col-6 d-flex justify-content-between" >
+    <div>
+    <a  className="text-white animated-x" href="tel:+91 8046601700/796"> Call us: +918046601700/796  </a>
+    </div>
+    <div >
+    
+    <a className="text-white animated-x" href="tel:+91 8046601700"><BiPhoneCall className='fs-4 mx-4 text-white'/></a>
+    
+    <a className="text-white animated-x" href="mailto:abhishek@dilabs.in"><AiOutlineMail className='fs-4 mx-4 text-white'/></a>
+    
+    
+    
+    </div>
+    </div> 
+   
   
+  </div>
+</div>
+  </header>
   </>;
 }
 
