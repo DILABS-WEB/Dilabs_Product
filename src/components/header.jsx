1478bfa6198e9730ba8e9ec09afeaa42950c022f
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2023-07-14 15:55:49 +0530

    Added animatedx functionality to the strip

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,22 +4,22 @@ import {AiOutlineMail}  from 'react-icons/ai'
 
 const header = () => {
   return <>
-  <header className='header-top-strip p-2 bg-danger'>
-<div className="container-xxl">
+  <header className='header-top-strip p-2 bg-primary'>
+<div className="container-xxl animated-right-left">
   <div className="row">
     <div className="col-6 text-white animated-right-left"  >
       <p>Shipping available with Additional charges of $10.</p>
       
     </div>
-    <div className="col-6 d-flex justify-content-between" >
-    <div>
-    <a  className="text-white animated-x" href="tel:+91 8046601700/796"> Call us: +918046601700/796  </a>
+    <div className="col-6 d-flex justify-content-between " >
+    <div >
+    <a  className="text-white " href="tel:+91 8046601700/796"> Call us: +918046601700/796  </a>
     </div>
     <div >
     
     <a className="text-white animated-x" href="tel:+91 8046601700"><BiPhoneCall className='fs-4 mx-4 text-white'/></a>
     
-    <a className="text-white animated-x" href="mailto:abhishek@dilabs.in"><AiOutlineMail className='fs-4 mx-4 text-white'/></a>
+    <a className="text-white " href="mailto:abhishek@dilabs.in"><AiOutlineMail className='fs-4 mx-4 text-white'/></a>
     
     
     
