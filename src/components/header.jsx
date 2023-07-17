b9633c629e575c7588449d2d79a8bb9dfb847d0f
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2023-07-17 14:55:49 +0530

    updated  till  updated header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import {BiPhoneCall} from  'react-icons/bi';
 import {AiOutlineMail}  from 'react-icons/ai';
+import {HiOutlineInboxIn}  from 'react-icons/hi';
+import {VscAccount}  from 'react-icons/vsc';
+import {CgShoppingCart}  from 'react-icons/cg';
 import {Link} from 'react-router-dom'
 import logo from "../assets/image/logo.jpg"
 
@@ -11,7 +14,7 @@ import logo from "../assets/image/logo.jpg"
 
 const header = () => {
   return <>
-  <header className='header-top-strip p-1 '>
+  <header className='header-top-strip p-1 px-4 shadow-mc'>
 <div className="container-xxl animated-right-left">
   <div className="row align-items-center">
     <div className="col-6 text-white "  >
@@ -40,20 +43,40 @@ const header = () => {
   <header className='header-upper'>
     <div className="container-xxl">
       <div className="row">
-     <div className="col-2">
+     <div className="col-2 m-auto text-center">
       <Link to='/'><img src={logo} alt="" srcset="" className='img-fluid logo' /></Link>
       
      </div>
-     <div className="col-5 d-flex align-items-center justify-content-between">
+     <div className="col-3 d-flex align-items-center mt-3">
+     <div className="input-group mb-3">
+     <span className="input-group-text" id="basic-addon1">All</span>
+  <input type="text" className="form-control p-2" placeholder="Enter Part No./ Keyword" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+
+    <button className="input-group-button" id="basic-addon2">Search</button>
+ 
+</div>
+</div>
+<div className="nav-links col-4 d-flex align-items-center justify-content-between text-center m-auto">
 <Link to={'/'} >HOME</Link>
 <Link to={'shop'} >SHOP</Link>
 <Link to={'about'} >ABOUT</Link>
 <Link to={'contact'} >CONTACT</Link>
-     </div>
-     <div className="col-5"></div>
-      </div>
-    </div>
-  </header>
+</div>
+<div className="nav-links-nav col-3 d-flex align-items-center justify-content-between">
+<Link className='d-flex'> <HiOutlineInboxIn className='fs-3 mx-2'/> 
+<p>Wishlist</p>
+</Link>
+<Link className='d-flex'> <VscAccount className='fs-3 mx-2'/> 
+<p>Account</p>
+</Link>
+<Link className='d-flex'> <CgShoppingCart className='fs-3 mx-2'/> 
+<p>Cart</p>
+</Link>
+
+</div>
+</div>
+</div>
+</header>
   </>;
 }
 
