75840dcf810f3e63dab36b82c052ba28623c67d6
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2023-07-18 10:24:50 +0530

    updated with header and footer and css

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React  from 'react'
 import {BiPhoneCall} from  'react-icons/bi';
 import {AiOutlineMail}  from 'react-icons/ai';
 import {HiOutlineInboxIn}  from 'react-icons/hi';
 import {VscAccount}  from 'react-icons/vsc';
 import {CgShoppingCart}  from 'react-icons/cg';
-import {Link} from 'react-router-dom'
+import {Link, useLocation} from 'react-router-dom'
 import logo from "../assets/image/Dilabs.png"
 
 
@@ -13,6 +13,7 @@ import logo from "../assets/image/Dilabs.png"
 
 
 const header = () => {
+  const location =useLocation();
   return <>
   <header className='header-top-strip p-1 px-4 shadow-mc'>
 <div className="container-xxl animated-right-left">
@@ -57,20 +58,26 @@ const header = () => {
 </div>
 </div>
 <div className="nav-links col-4 d-flex align-items-center justify-content-between text-center m-auto">
-<Link to={'/'} >Home</Link>
-<Link to={'shop'} >Shop</Link>
-<Link to={'about'} >About</Link>
-<Link to={'contact'} >Contact</Link>
+<Link to={'/'} className={location.pathname === '/' ? 'active' : 'inactive'} >Home</Link>
+<Link to={'shop'} className={location.pathname === '/shop' ? 'active' : 'inactive'} >Shop</Link>
+<Link to={'about'} className={location.pathname === '/about' ? 'active' : 'inactive'} >About</Link>
+<Link to={'contact'} className={location.pathname === '/contact' ? 'active' : 'inactive'} >Contact</Link>
 </div>
 <div className="nav-links-nav col-3 d-flex align-items-center justify-content-between">
-<Link to={'/wishlist'} className='d-flex'> <HiOutlineInboxIn className='fs-3 mx-2'/> 
+
+<Link to={'wishlist'} className={location.pathname === '/wishlist' ? 'active' : 'inactive'}> <div className="d-flex"> <span><HiOutlineInboxIn className='fs-3 mx-2'/> 
+</span>
 <p>Wishlist</p>
+</div>
 </Link>
-<Link to={'/login'} className='d-flex'> <VscAccount className='fs-3 mx-2'/> 
+
+<Link to={'login'} className={location.pathname === '/login' ? 'active' : 'inactive'}>  <div className='d-flex'> <span><VscAccount className='fs-3 mx-2'/> </span> 
 <p>Account</p>
+</div>
 </Link>
-<Link to={'cart'} className='d-flex'> <CgShoppingCart className='fs-3 mx-2'/> 
+<Link to={'cart'} className={location.pathname === '/cart' ? 'active' : 'inactive'}>  <div className='d-flex'> <span><CgShoppingCart className='fs-3 mx-2'/> </span> 
 <p>Cart</p>
+</div>
 </Link>
 
 </div>
