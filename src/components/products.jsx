378852d7d8064b5bda490d783e5c1c4fb1042c02
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2023-07-18 14:55:05 +0530

    added with featured Product

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,6 +1,143 @@
-export default PRODUCTS =[
+import img1 from '../assets/image/Products/Inertial Navigation System/insb.png'
+import img2 from '../assets/image/Products/Inertial Navigation System/insp.png'
+import img3 from '../assets/image/Products/Inertial Navigation System/insdd.png'
+import img4 from '../assets/image/Products/Inertial Navigation System/ins_dl.png'
+import img5 from '../assets/image/Products/Inertial Navigation System/imu_p.png'
+import img6 from '../assets/image/Products/Inertial Navigation System/DTS.png'
+import img7 from '../assets/image/Products/Inertial Navigation System/ahrs1.png'
+import img8 from '../assets/image/Products/Inertial Navigation System/AHRS.png'
+import img9 from '../assets/image/Products/Inertial Navigation System/mru_b2.png'
+import img10 from '../assets/image/Products/Inertial Navigation System/mru_e.png'
+import img11 from '../assets/image/Products/Inertial Navigation System/mru-pd.png'
+import img12 from '../assets/image/Products/Inertial Navigation System/optoahrs.png'
+import img13 from '../assets/image/Products/Inertial Navigation System/p1.png'
+import img14 from '../assets/image/Products/Inertial Navigation System/ws-e.png'
+import img15 from '../assets/image/Products/Inertial Navigation System/ws-pd.png'
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const PRODUCTS = [
     {
         id:0,
-        
-    }
-]
+        Image:img1,
+        partnumber:'DILABS000000',
+        price:20,
+        status:'in stock',
+        title:'INS-B-OEM Basic',
+        },
+        {
+            id:1,
+            Image:img2,
+            partnumber:'DILABS000001',
+            price:30,
+            status:'in stock',
+            title:'INS-P Professional',
+        },
+    {
+        id:2,
+        Image:img3,
+        partnumber:'DILABS000002',
+        price:20,
+        status:'in stock',
+        title:'INS-D',
+        },
+        {
+            id:3,
+            Image:img4,
+            partnumber:'DILABS000003',
+            price:30,
+            status:'in stock',
+            title:'INS-DL',
+        },
+    {
+        id:4,
+        Image:img5,
+        partnumber:'DILABS000004',
+        price:20,
+        status:'in stock',
+        title:'IMU - P',
+        },
+        {
+            id:5,
+            Image:img6,
+            partnumber:'DILABS000005',
+            price:30,
+            status:'in stock',
+            title:'OS3D-DTS',
+        },
+    {
+        id:6,
+        Image:img7,
+        partnumber:'DILABS000006',
+        price:20,
+        status:'in stock',
+        title:'AHRS-10B',
+        },
+        {
+            id:7,
+            Image:img8,
+            partnumber:'DILABS000007',
+            price:30,
+            status:'in stock',
+            title:'AHRS-10P',
+        },
+    {
+        id:8,
+        Image:img9,
+        partnumber:'DILABS000008',
+        price:20,
+        status:'in stock',
+        title:'MRU-B Basic',
+        },
+        {
+            id:9,
+            Image:img10,
+            partnumber:'DILABS000009',
+            price:30,
+            status:'in stock',
+            title:'MRU-E Enhanced',
+        },
+    {
+        id:10,
+        Image:img11,
+        partnumber:'DILABS000010',
+        price:20,
+        status:'in stock',
+        title:'MRU-P Professional',
+        },
+        {
+            id:11,
+            Image:img12,
+            partnumber:'DILABS000011',
+            price:30,
+            status:'in stock',
+            title:'OptoAHRS-II',
+        },
+    {
+        id:12,
+        Image:img13,
+        partnumber:'DILABS000012',
+        price:20,
+        status:'in stock',
+        title:'INS-D-OEM,INS-DL-OEM',
+        },
+        {
+            id:13,
+            Image:img14,
+            partnumber:'DILABS000013',
+            price:30,
+            status:'in stock',
+            title:'Wave Sensor WS-E Enhanced',
+        },
+    {
+        id:14,
+        Image:img15,
+        partnumber:'DILABS000014',
+        price:20,
+        status:'in stock',
+        title:'Wave Sensor WS-PD Professional Dual',
+        },
+      
+    
+    
+       
+];
