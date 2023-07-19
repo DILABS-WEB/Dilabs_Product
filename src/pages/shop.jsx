e1f9a165a67bae9bb04b3127144666c049912a0d
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2023-07-19 12:24:03 +0530

    shop updated

--- src/pages/shop.jsx
+++ src/pages/shop.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
 const shop = () => {
-  return (
-    <div>shop</div>
-  )
+  return <>
+   <section className='shop-banner1 p-2'>
+    <div className="container-xxl">
+      <div className="row justify-content-center align-items-center ">
+        <div className="banner1-details text-center p-5">
+       <div>
+       <h1 className='text-danger mt-3'>We Provide All Kind of Of Military & Avionics Applications</h1>
+       </div>
+       <div>
+        <h3 className='text-light'>Make Your Order </h3>
+       </div>
+        
+        </div>
+      </div>
+    </div>
+  </section>
+  
+  </>;
 }
 
 export default shop
