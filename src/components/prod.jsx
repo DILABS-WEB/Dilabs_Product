bcb4187b5cf98c322c362560bfb8370c9246143f
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2023-07-19 15:03:14 +0530

    added prod

--- src/components/prod.jsx
+++ src/components/prod.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {ShopContext} from './shopcontext';
- 
+import ReactStars from "react-rating-stars-component";
 
 
 const prod = (props) => {
@@ -9,8 +9,24 @@ const prod = (props) => {
   <div key={id} className="col-3">
     <div className="card">
         <img src={Image} alt="..."  className='img-fluid card-img-top'/>
-       <div className="card-details">
-<h2>{name}</h2>
+       <div className="card-details p-4">
+        <span>{partnumber}</span>
+<h5 className='my-3'>{name}</h5>
+ <ReactStars
+    count={5}
+    size={24}
+    edit={false}
+    value={4}
+    activeColor="#cca27e"
+    />
+    
+    <p className='price'>{price}</p>
+   
+<div className='d-flex align-items-center'>
+<button>View Details</button>
+       <button>Add To Cart</button>
+</div>
+      
        </div>
     </div>
   </div>
