ff2f3bd30cd01251eee653bfa6a54dd412023131
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2023-07-19 15:40:38 +0530

    add with the prod and shop

--- src/components/prod.jsx
+++ src/components/prod.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {ShopContext} from './shopcontext';
 import ReactStars from "react-rating-stars-component";
+import { Link } from 'react-router-dom';
 
 
 const prod = (props) => {
     const{id,name,Image,partnumber,price} = props.data;
+    const {viewProductDetails,addToCart,cartItems}=useContext(ShopContext);
+    const cartitemCount=cartItems[id];
   return <>
   <div key={id} className="col-3">
-    <div className="card">
+    <div className="card mb-4">
         <img src={Image} alt="..."  className='img-fluid card-img-top'/>
        <div className="card-details p-4">
         <span>{partnumber}</span>
@@ -20,11 +23,17 @@ const prod = (props) => {
     activeColor="#cca27e"
     />
     
-    <p className='price'>{price}</p>
+    <p className='price mb-3'>{price}</p>
    
-<div className='d-flex align-items-center'>
-<button>View Details</button>
-       <button>Add To Cart</button>
+<div className='d-flex align-items-center justify-content-around mb-2'>
+<div className="col-6">
+<Link className='view-button' onClick={()=>(viewProductDetails(id))}>View Details</Link>
+</div>
+
+<div className="col-6">
+<button className='add-button' onClick={()=>(addToCart(id))}>Add To Cart{cartitemCount>0 && `(${cartitemCount})` }</button>
+</div>
+       
 </div>
       
        </div>
