b4e4e25cf37b91410da0f8d7b74ec9765d877eee
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2023-07-20 12:38:36 +0530

    updated with the cart jsx

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,21 +1,35 @@
-import React, { useContext } from 'react'
-import { PRODUCTS, PRODUCTS1 } from '../components/products';
-import CartItems from '../components/cartitems';
-import ShopContext from '../components/shopcontext';
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable react/jsx-key */
+/* eslint-disable no-unused-vars */
+import React, { useContext, useState } from 'react'
+import { PRODUCTS } from '../components/products'
+import { PRODUCTS1 } from '../components/products'
+import { ShopContext } from '../components/shopcontext'
+import CartItem from '../components/cartitems'
 
 
-const cart = () => {
-    const [cartItems] = useContext(ShopContext);
+
+const cart = (props) => {
+    const { cartItems, getTotalCartAmount, clearCart } = useContext(ShopContext);
+
+
+
+  
   return <>
+  <section className="cart">
+
     <div className="row">
-    {[...PRODUCTS, ...PRODUCTS1].map((product)=>{
-if (cartItems[product.id] !== 0){
-  return <CartItems key={product.id} data={product}/>
-}
-})}
-  </div>
-  </>
- 
+    {[...PRODUCTS, ...PRODUCTS1].map((product) => {
+        if (cartItems[product.id] !== 0) {
+            return <CartItem key={product.id} data={product} />;
+        }
+        })}
+        
+    
+    
+    </div>
+  </section>
+  </>;
 }
 
 export default cart
