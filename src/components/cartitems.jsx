a7c3a9bf202c620fce4b309eabc14231f9ccb21d
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2023-07-20 14:27:34 +0530

    updated with the details

--- src/components/cartitems.jsx
+++ src/components/cartitems.jsx
@@ -1,40 +1,51 @@
-import React from 'react'
-import { useContext } from 'react';
-import Shopcontext from './shopcontext';
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable no-unused-vars */
+import React, { useContext} from 'react'
+import { ShopContext } from './shopcontext'
+import { RiDeleteBack2Line } from 'react-icons/ri'
+const cartitem = (props) => {
 
-
-const cartitems = (props) => {
     const{id,name,Image,partnumber,price} = props.data;
-   
+    const { addToCart, cartItems, removeToCart, updateCartItemCount, itemPrice} = useContext(ShopContext);
+    const itemsInStock = id === 0 ? Math.ceil((id + 2.5) * 102 / 2) : Math.ceil((id * 102.5) / 2);
   return <>
-  <section className="cart-item p-5">
-<div className="container-xxl">
-    <div  className="row">
-        <table>
-            <thead>
-                <th>Product Image</th>
-                <th>Product Details</th>
-                <th>Edit</th>
-                <th>Coupons</th>
-            </thead>
-            <tbody>
-                <tr>
-                   <td>
-                    <img src={Image} alt="..."   className='img-fluid' />
-                    </td> 
-                    <td>
-                        <p>{partnumber}</p>
-                        <p>{name}</p>
-                        <p>{price}</p>
 
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+
+<div className="container card my-3">
+      <div className="row g-3">
+        <div className="col-12 col-md-5">
+          <div className="p-3">
+            <div className="cart-item-image m-auto">
+              <img src={Image} className="card-img-top img-fluid" alt="..." />
+            </div>
+          </div>
+        </div>
+        <div className="col-12 col-md-7">
+          <div className="p-3">
+            <h2>{name}</h2>
+            <p className="cart-item-id">Product PartNumber: <b className='text-center mb-1'>{partnumber}</b></p>
+            <p className="cart-item-id">Product Price: <b className='text-center mb-1'>${price}</b></p>
+            <p className="cart-item-id">Product Price INR: <b className='text-center mb-1'>{82.05*price}</b></p>
+            <p className="cart-item-id">Product Number: <b className='text-center mb-3'>{id}</b></p>
+            <p className="cart-item-id">Items in Stock: <b className='text-danger'>{itemsInStock}</b></p>
+          </div>
+          <div className="p-3 d-flex justify-content-between align-items-center">
+            <div className="count-handler">
+              <button className="btn btn-outline-secondary" onClick={() => addToCart(id)}>+</button>
+              <input className='text-danger fs-4 form-control' value={cartItems[id]} onChange={(e) => updateCartItemCount(Number(e.target.value), id)} />
+              <button className="btn btn-outline-secondary" onClick={() => removeToCart(id)}>-</button>
+            </div>
+            <button className="btn btn-outline-danger" onClick={() => removeToCart(id)}>
+              <RiDeleteBack2Line />
+            </button>
+          </div>
+          <div className="p-3">
+            <input type="text" className="form-control" id="coupon" placeholder="Enter coupon code..." />
+          </div>
+        </div>
+      </div>
     </div>
-</div>
-  </section>
   </>;
 }
 
-export default cartitems
+export default cartitem
